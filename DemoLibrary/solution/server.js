50f761efb86ce1c3eb386540c4735fb7319532ac
surabhi8 <surabhigupta2442@gmail.com>
2018-02-05 10:20:52 +0530

    Running server on port 8080

---
+++ DemoLibrary/solution/server.js
@@ -0,0 +1,17 @@
+const Hapi =  require('hapi')
+const server = new  Hapi.Server
+server.connection({
+    host: 'localhost',
+    port: 8080,
+  });
+
+if(!module.parent){
+    server.start ( (err) => {
+        if(err){
+            throw err;
+        } 
+        console.log("Server started")
+    }
+)
+}
+module.exports = server
