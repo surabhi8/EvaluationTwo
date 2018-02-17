25c811bb7ea101e64d7fb15a401ee5a8317fc563
surabhi8 <surabhigupta2442@gmail.com>
2018-02-18 02:38:35 +0530

    Part 1 code modularized

--- src/solution/routes/api.js
+++ src/solution/routes/api.js
@@ -1,38 +1,59 @@
 const rp = require('request-promise');
 
+const getAllBooksArray = () => {
+  const promise1 = rp('https://5gj1qvkc5h.execute-api.us-east-1.amazonaws.com/dev/allBooks').then(htmlString => htmlString);
+  return (promise1.then((value) => {
+    const allBooksArray = JSON.parse(value).books;
+    return allBooksArray;
+  }));
+};
+const getAllBooksRatings = (allBooksArray) => {
+  const promiseArray = [];
+  for (let i = 0; i < allBooksArray.length; i += 1) {
+    const promise3 = rp(`https://5gj1qvkc5h.execute-api.us-east-1.amazonaws.com/dev/findBookById/${allBooksArray[i].id}`).then(htmlString => htmlString);
+    promiseArray.push(promise3);
+  }
+  return Promise.all(promiseArray);
+};
+const getAllBooksWithRatings = (allBooksArray, allBooksRatings) => {
+  const allBooksWithRatings = [];
+  for (let i = 0; i < allBooksArray.length; i += 1) {
+    allBooksWithRatings.push({
+      author: allBooksArray[i].Author,
+      id: allBooksArray[i].id,
+      name: allBooksArray[i].Name,
+      rating: JSON.parse(allBooksRatings[i]).rating,
+    });
+  }
+  return allBooksWithRatings;
+};
+const getAllBooksGroupedByAuthor = (allBooksWithRatings) => {
+  const finalOutput = {};
+  for (let i = 0; i < allBooksWithRatings.length; i += 1) {
+    if (typeof finalOutput[allBooksWithRatings[i].author] === 'undefined') {
+      finalOutput[allBooksWithRatings[i].author] = [];
+    }
+    finalOutput[allBooksWithRatings[i].author].push({
+      author: allBooksWithRatings[i].author,
+      id: allBooksWithRatings[i].id,
+      name: allBooksWithRatings[i].name,
+      rating: allBooksWithRatings[i].rating,
+    });
+  }
+  return finalOutput;
+};
 module.exports = {
-  path: '/getAllBooks',
+  path: '/Books/BooksWithRatings',
   method: 'GET',
-
   handler(request, reply) {
-    const promise1 = rp('https://5gj1qvkc5h.execute-api.us-east-1.amazonaws.com/dev/allBooks').then(htmlString => htmlString);
-    promise1.then((value) => {
-      const resultID = [];
-      const parse = JSON.parse(value).books;
-      for (let i = 0; i < parse.length; i += 1) {
-        resultID.push(parse[i].id);
-      }
-      const promiseArray = [];
-      for (let i = 0; i < resultID.length; i += 1) {
-        const promise3 = rp(`https://5gj1qvkc5h.execute-api.us-east-1.amazonaws.com/dev/findBookById/${resultID[i]}`).then(htmlString => htmlString);
-        promiseArray.push(promise3);
-      }
-      Promise.all(promiseArray).then((values) => {
-        const finalOutput = {};
-        for (let i = 0; i < parse.length; i += 1) {
-          if (typeof finalOutput[parse[i].Author] === 'undefined') {
-            finalOutput[parse[i].Author] = [];
-          }
-          finalOutput[parse[i].Author].push({
-            author: parse[i].Author,
-            id: parse[i].id,
-            name: parse[i].Name,
-            rating: JSON.parse(values[i]).rating,
-          });
-        }
+    getAllBooksArray().then((allBooksArray) => {
+      getAllBooksRatings(allBooksArray).then((allBooksRatings) => {
+        const allBooksWithRatings = getAllBooksWithRatings(allBooksArray, allBooksRatings);
+        const final = getAllBooksGroupedByAuthor(allBooksWithRatings);
+        const finalOutput = final;
+        console.log('Hello', finalOutput);
         reply({ data: finalOutput, statusCode: 200 });
       });
     });
   },
 };
-
